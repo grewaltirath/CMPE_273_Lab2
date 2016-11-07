c3e826ddcbd5cb87a6997ea1f631e902b6a11576
grewaltirath <tirathsingh.grewal@sjsu.edu>
2016-11-06 16:18:47 -0800

    Update index.js

--- Ebay_test_server/routes/index.js
+++ Ebay_test_server/routes/index.js
@@ -8,23 +8,3 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-/*module.exports = function(passport){
-
-	 
-	  /* Handle Login POST */
-//	  router.post('/login', passport.authenticate('login', {
-//	    successRedirect: '/home',
-//	    failureRedirect: '/',
-//	    failureFlash : true 
-//	  }));
-	
-	 
-	  /* Handle Registration POST */
-//	  router.post('/signup', passport.authenticate('signup', {
-//	    successRedirect: '/',
-//	    failureRedirect: '/insertUser',
-//	    failureFlash : true 
-//	  }));
-//	 
-	//  return router;
-	//}; */
